8422a883697b292c63edeb809e641a18346dc338
Micheal Limones <michlimones@gmail.com>
2021-10-13 08:33:23 -0600

    added modal to project 1.

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,18 @@ contactMeLink.onclick = function () {
   contactMeSection.classList.remove('hide');
   contactMeSection.classList.add('show');
 };
+
+var modal = document.querySelector('.project-modal');
+var btn = document.querySelector('.details-btn');
+var span = document.getElementsByClassName('close')[0];
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
